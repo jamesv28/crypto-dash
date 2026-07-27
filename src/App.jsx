b6e741e12c17309bc28fac051d6307fcd48adf5e
jamesv28 <jamesvolmert@Jamess-iMac.local>
2026-07-27 11:58:17 -0600

    added filtering capability

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
 const API_URL = import.meta.env.VITE_API_URL;
-("");
+
 function App() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
         const res = await fetch(
-          `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`,
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`,
         );
         if (!res.ok) throw new Error("Failed to load data");
         const data = await res.json();
@@ -25,10 +27,11 @@ function App() {
     };
 
     fetchCoins();
-  }, []);
+  }, [limit]);
   return (
     <div>
       <h2>&#128640; Crypto Dash</h2>
+      <LimitSelector limit={limit} onLimitChange={setLimit} />
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error} </div>}
       {!loading && !error && (
