7d5c73e1c2904a0e3c41b7764ff98507ea28fab7
jamesv28 <jamesvolmert@Jamess-iMac.local>
2026-07-27 16:55:19 -0600

    added sorting

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
 import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
 const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
   const [error, setError] = useState(null);
   const [limit, setLimit] = useState(10);
   const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
+
   useEffect(() => {
     const fetchCoins = async () => {
       try {
@@ -18,7 +21,6 @@ function App() {
         );
         if (!res.ok) throw new Error("Failed to load data");
         const data = await res.json();
-        console.log("coins", data);
         setCoins(data);
       } catch (error) {
         setError(error.message);
@@ -29,18 +31,41 @@ function App() {
 
     fetchCoins();
   }, [limit]);
-  const filteredCoins = coins.filter((coin) => {
-    return (
-      coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-      coin.symbol.toLowerCase().includes(filter.toLowerCase())
-    );
-  });
+  const filteredCoins = coins
+    .filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(filter.toLowerCase())
+      );
+    })
+    .slice()
+    .sort((a, b) => {
+      switch (sortBy) {
+        case "market_cap_desc":
+          return b.market_cap - a.market_cap;
+        case "market_cap_asc":
+          return a.market_cap - b.market_cap;
+        case "price_desc":
+          return b.current_price - a.current_price;
+        case "price_asc":
+          return a.current_price - b.current_price;
+        case "change_desc":
+          return (
+            b.price_cahange_percentage_24hr - a.price_cahange_percentage_24hr
+          );
+        case "change_asc":
+          return (
+            a.price_cahange_percentage_24hr - b.price_cahange_percentage_24hr
+          );
+      }
+    });
   return (
     <div>
       <h2>&#128640; Crypto Dash</h2>
       <div className="top-controls">
         <FilterInput filter={filter} onFilterChange={setFilter} />
         <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
       </div>
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error} </div>}
