55eb28e2705b5ddcce710ea80d43c066c9b9884c
jamesv28 <jamesvolmert@Jamess-iMac.local>
2026-07-27 11:43:21 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
+const API_URL = import.meta.env.VITE_API_URL;
+("");
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`,
+        );
+        if (!res.ok) throw new Error("Failed to load data");
+        const data = await res.json();
+        console.log("coins", data);
+        setCoins(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, []);
+  return (
+    <div>
+      <h2>&#128640; Crypto Dash</h2>
+      {loading && <p>Loading...</p>}
+      {error && <div className="error">{error} </div>}
+      {!loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <CoinCard coin={coin} key={coin.id} />
+          ))}
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default App;
